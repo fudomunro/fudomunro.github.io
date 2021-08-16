8cfd0217d23e74552dd7e2b851c021671d0a5a57
Alec Munro <alecmunro@gmail.com>
2021-08-16 14:56:04 -0300

    Adding some sample tests

--- cypress/integration/first.js
+++ cypress/integration/first.js
@@ -1,12 +1,39 @@
 //const cypress = require("cypress")
 
 describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(true)
-  })
   
   it('Can load the page', () => {
     cy.visit("/test.html")
     cy.contains("The Page").should('be.visible')
   })
+  
+  it('Can have a timing issue', () => {
+    cy.visit("/test.html")
+    
+    cy.get("action-button").click()
+    
+    cy.get("updated-area").should('have.text', 'Update Successful')
+  })
+  
+  it('Can have clicks get intercepted', () => {
+    cy.visit("/test.html")
+    
+    cy.get('open-modal').click()
+    
+    cy.get("overlapped-button").click()
+    
+    cy.get("updated-area").should('have.text', 'Update Successful')
+  })
+  
+  it('Can be hard to find a good selector', () => {
+    cy.visit("/test.html")
+    
+    // We're trying to press a particular button, but can't find a good selector for it
+    // Page will include buttons with some kind of automatically generated identifiers (or at least they can look that way)
+    // Target button will not be a button at all, but rather a div with some text
+    
+    // Help! This worked, but then the page changed, and now it's failing!
+    cy.get('html body div div p button').click()
+  })
+  
 })
